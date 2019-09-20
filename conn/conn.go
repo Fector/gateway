8858799fd22a03da3e2bf7c0f597ab1c57e957a3
DeathHand <stmaxonline@gmail.com>
2019-09-20 17:14:33 +0300

    implementation

--- conn/conn.go
+++ conn/conn.go
@@ -5,32 +5,47 @@ import (
 	"fmt"
 	"github.com/DeathHand/gateway/model"
 	"github.com/DeathHand/smpp/pdu"
+	"github.com/DeathHand/smpp/protocol"
+	"log"
 	"net"
+	"sync/atomic"
 	"time"
 )
 
 type Conn struct {
 	*net.TCPConn
-	rx      *Receiver
-	tx      *Transmitter
-	gateway *model.Gateway
-	context *Context
-	inbox   *chan model.Message
-	outbox  *chan model.Message
-	timer   *time.Ticker
-	stop    *chan int
-	error   *chan error
+	SequenceNumber *uint32
+	LastPacketTime *int64
+	rx             *protocol.Receiver
+	tx             *protocol.Transmitter
+	gateway        *model.Gateway
+	inbox          *chan model.Message
+	outbox         *chan model.Message
+	timer          *time.Ticker
+	stop           *chan int
+	error          *chan error
 }
 
 func NewConnection(gateway *model.Gateway, inbox *chan model.Message, outbox *chan model.Message, stop *chan int) (*Conn, error) {
 	return &Conn{
 		gateway: gateway,
-		context: &Context{},
 		inbox:   inbox,
 		outbox:  outbox,
 		timer:   time.NewTicker(time.Duration(gateway.EnquireLinkTime) * time.Second),
 		stop:    stop,
 	}, nil
+}
+
+func (c *Conn) NextSequence() uint32 {
+	return atomic.AddUint32(c.SequenceNumber, 1)
+}
+
+func (c *Conn) UpdateTime() {
+	atomic.StoreInt64(c.LastPacketTime, time.Now().UnixNano())
+}
+
+func (c *Conn) Log(v ...interface{}) {
+	log.Println(fmt.Sprintf("[gateway:%s] ", c.gateway.Name), v)
 }
 
 func (c *Conn) Connect() error {
@@ -47,13 +62,118 @@ func (c *Conn) Connect() error {
 	c.TCPConn = tcpConn
 	r := make(chan pdu.Pdu, c.gateway.ReadChanSize)
 	t := make(chan pdu.Pdu, c.gateway.WriteChanSize)
-	c.rx = NewReceiver(tcpConn, &r)
-	c.tx = NewTransmitter(tcpConn, &t)
+	c.rx = protocol.NewReceiver(tcpConn, &r)
+	c.tx = protocol.NewTransmitter(tcpConn, &t)
+	return nil
+}
+
+func (c *Conn) getBindPdu() (pdu.Pdu, error) {
+	switch c.gateway.BindMode {
+	case protocol.BindModeRX:
+		return &pdu.BindReceiver{
+			Header: &pdu.Header{
+				CommandId:      protocol.BindReceiver,
+				CommandStatus:  protocol.EsmeRok,
+				SequenceNumber: c.NextSequence(),
+			},
+			Body: &pdu.BindBody{
+				SystemId:         c.gateway.SystemId,
+				Password:         c.gateway.Password,
+				SystemType:       c.gateway.SystemType,
+				InterfaceVersion: c.gateway.InterfaceVersion,
+				AddrTon:          c.gateway.AddrTon,
+				AddrNpi:          c.gateway.AddrNpi,
+				AddressRange:     c.gateway.AddressRange,
+			},
+			Tlv: nil,
+		}, nil
+	case protocol.BindModeTX:
+		return &pdu.BindTransmitter{
+			Header: &pdu.Header{
+				CommandId:      protocol.BindTransmitter,
+				CommandStatus:  protocol.EsmeRok,
+				SequenceNumber: c.NextSequence(),
+			},
+			Body: &pdu.BindBody{
+				SystemId:         c.gateway.SystemId,
+				Password:         c.gateway.Password,
+				SystemType:       c.gateway.SystemType,
+				InterfaceVersion: c.gateway.InterfaceVersion,
+				AddrTon:          c.gateway.AddrTon,
+				AddrNpi:          c.gateway.AddrNpi,
+				AddressRange:     c.gateway.AddressRange,
+			},
+			Tlv: nil,
+		}, nil
+	case protocol.BindModeTRX:
+		return &pdu.BindTransceiver{
+			Header: &pdu.Header{
+				CommandId:      protocol.BindTransceiver,
+				CommandStatus:  protocol.EsmeRok,
+				SequenceNumber: c.NextSequence(),
+			},
+			Body: &pdu.BindBody{
+				SystemId:         c.gateway.SystemId,
+				Password:         c.gateway.Password,
+				SystemType:       c.gateway.SystemType,
+				InterfaceVersion: c.gateway.InterfaceVersion,
+				AddrTon:          c.gateway.AddrTon,
+				AddrNpi:          c.gateway.AddrNpi,
+				AddressRange:     c.gateway.AddressRange,
+			},
+			Tlv: nil,
+		}, nil
+	}
+	return nil, errors.New("Unknown bind mode ")
+}
+
+func (c *Conn) checkBindResp(header *pdu.Header) error {
+	if header.CommandStatus != protocol.EsmeRok {
+		return errors.New(
+			fmt.Sprintf("Bind failed with code: %s", protocol.GetStatusName(header.CommandStatus)),
+		)
+	}
+	return nil
+}
+
+func (c *Conn) Bind() error {
+	req, err := c.getBindPdu()
+	if err != nil {
+		return err
+	}
+	err = c.SetDeadline(time.Now().Add(time.Duration(c.gateway.BindTimeout) * time.Second))
+	if err != nil {
+		return err
+	}
+	_, err = c.tx.W.WritePdu(&req)
+	if err != nil {
+		return err
+	}
+	err = c.SetDeadline(time.Now().Add(time.Duration(c.gateway.BindTimeout) * time.Second))
+	if err != nil {
+		return err
+	}
+	p, err := c.rx.R.ReadPacket()
+	if err != nil {
+		return err
+	}
+	resp, err := c.rx.R.ReadPdu(p)
+	switch p := resp.(type) {
+	case pdu.BindReceiverResp:
+		return c.checkBindResp(p.Header)
+	case pdu.BindTransceiverResp:
+		return c.checkBindResp(p.Header)
+	case pdu.BindTransmitterResp:
+		return c.checkBindResp(p.Header)
+	}
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (c *Conn) handlePdu(p *pdu.Pdu) {
-	*c.tx.t <- p
+	*c.tx.P <- p
 }
 
 func (c *Conn) handleMessage(m *model.Message) {
@@ -71,7 +191,7 @@ func (c *Conn) Run() error {
 		select {
 		case err := <-*c.error:
 			return err
-		case p := <-*c.rx.r:
+		case p := <-*c.rx.P:
 			go c.handlePdu(&p)
 		case m := <-*c.inbox:
 			go c.handleMessage(&m)
